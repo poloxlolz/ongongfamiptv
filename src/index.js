bf75985773677ff12d972c5976bb69285733d87b
Kah Sha ONG <ong_kah_sha@htx.gov.sg>
2025-07-05 14:59:19 +0800

    Sync Worker code from Cloudflare dashboard

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 export default {
   async fetch(request) {
-    const wallpapers = [
-      "https://w.wallhaven.cc/full/po/wallhaven-po2vg3.jpg",
-      "https://w.wallhaven.cc/full/je/wallhaven-je82zp.jpg"
-    ];
-    const randomUrl = wallpapers[Math.floor(Math.random() * wallpapers.length)];
-    return Response.redirect(randomUrl, 302);
+
+    const response = await fetch("https://wallhaven.cc/api/v1/search?q=landscape&categories=010&purity=100&atleast=1920x1080&ratios=16x9&sorting=random");
+    if (!response.ok) {
+      return new Response("Failed to fetch from Wallhaven", { status: 500 });
+    }
+
+    const json = await response.json();
+    if (!json.data || json.data.length === 0) {
+      return new Response("No images found", { status: 404 });
+    }
+
+    return Response.redirect(json.data[0].path, 302);
   }
-}
+};
